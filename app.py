429071a3616c0e6f138f59074bfabd7e93f4d9ac
Ubuntu <ubuntu@ip-172-31-29-33.us-east-2.compute.internal>
2019-12-23 01:50:38 +0000

    fixed id names for lower case search functionality

--- app.py
+++ app.py
@@ -210,4 +210,4 @@ def post_search_title():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT'
-            )), debug=False)
+            )), debug=True)
